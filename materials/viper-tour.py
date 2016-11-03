91ea56aaed8a87ebec8f33ed88f5d1aaa0618725
Chen KunYu <18z@users.noreply.github.com>
2016-11-03 15:29:37 +0800

    Create viper-tour.py

---
+++ materials/viper-tour.py
@@ -0,0 +1,129 @@
+### Viper tour
+
+1. viper.py -> console.py -> colors.py
+
+2. viper.py -> console.py -> session.py -> out.py -> colors.py
+
+3. viper.py -> console.py -> objects.py
+
+4. viper.py -> console.py -> plugins.py -> abstracts.py -> colors.py
+
+5. viper.py -> console.py -> plugins.py -> module/fuzzy.py -> out.py -> colors.py
+
+6. viper.py -> console.py -> plugins.py -> module/fuzzy.py -> abstracts.py -> colors.py
+
+7. viper.py -> console.py -> plugins.py -> modules/fuzzy.py -> database.py -> out.py -> colors.py
+
+8. viper.py -> console.py -> plugins.py -> modules/fuzzy.py -> database.py -> objects.py
+
+9. viper.py -> console.py -> plugins.py -> modules/fuzzy.py -> session.py -> out.py -> colors.py
+
+10. viper.py console.py plugins.py modules/fuzzy.py session.py objects.py 
+
+11. viper.py console.py plugins.py modules/pe.py out.py colors.py
+
+12. viper.py console.py plugins.py modules/pe.py objects.py
+
+13. viper.py console.py plugins.py modules/pe.py abstracts.py colors.py
+
+14. viper.py console.py plugins.py modules/pe.py session.py out.py colors.py 
+
+15. viper.py console.py plugins.py modules/pe.py session.py objects.py
+
+16. viper.py console.py plugins.py modules/strings.py out.py colors.py
+
+17. viper.py console.py plugins.py modules/strings.py abstracts.py colors.py
+
+18. viper.py console.py plugins.py modules/strings.py session.py out.py colors.py
+
+19. viper.py console.py plugins.py modules/strings.py session.py objects.py 
+
+20. viper.py console.py plugins.py modules/virustotal.py out.py colors.py
+
+21. viper.py console.py plugins.py modules/virustotal.py abstracts.py colors.py
+
+22. viper.py console.py plugins.py modules/virustotal.py session.py out.py colors.py
+
+23. viper.py console.py plugins.py modules/virustotal.py session.py objects.py
+
+24. viper.py console.py plugins.py modules/yarascan.py out.py colors.py
+
+25. viper.py console.py plugins.py modules/yarascan.py abstracts.py colors.py
+
+26. viper.py console.py plugins.py modules/yarascan.py database.py out.py colors.py
+
+27. viper.py console.py plugins.py modules/yarascan.py database.py objects.py
+
+28. viper.py console.py plugins.py modules/yarascan.py session.py out.py colors.py
+
+29. viper.py console.py plugins.py modules/yarascan.py session.py objects.py
+ 
+30. viper.py console.py plugins.py modules/yarascan.py storage.py 
+
+31. viper.py console.py commands.py out.py colors.py
+
+32. viper.py console.py commands.py colors.py
+
+33. viper.py console.py commands.py session.py out.py colors.py
+
+34. viper.py console.py commands.py session.py objects.py
+
+35. viper.py console.py commands.py plugins.py abstracts.py colors.py
+
+36. viper.py console.py commands.py plugins.py modules/fuzzy.py out.py colors.py
+
+37. viper.py console.py commands.py plugins.py modules/fuzzy.py abstracts.py colors.py
+
+38. viper.py console.py commands.py plugins.py modules/fuzzy.py database.py out.py colors.py
+
+39. viper.py console.py commands.py plugins.py modules/fuzzy.py database.py objects.py 
+
+40. viper.py console.py commands.py plugins.py modules/fuzzy.py session.py out.py colors.py
+
+41. viper.py console.py commands.py plugins.py modules/fuzzy.py session.py objects.py
+
+42. viper.py console.py commands.py plugins.py modules/pe.py out.py colors.py
+
+43. viper.py console.py commands.py plugins.py modules/pe.py objects.py
+
+44. viper.py console.py commands.py plugins.py modules/pe.py abstracts.py colors.py
+
+45. viper.py console.py commands.py plugins.py modules/pe.py session.py out.py colors.py
+
+46. viper.py console.py commands.py plugins.py modules/pe.py session.py objects.py
+
+47. viper.py console.py commands.py plugins.py modules/strings.py out.py colors.py
+
+48. viper.py console.py commands.py plugins.py modules/strings.py abstracts.py colors.py
+
+49. viper.py console.py commands.py plugins.py modules/strings.py session.py out.py colors.py
+
+50. viper.py console.py commands.py plugins.py modules/strings.py session.py objects.py
+
+51. viper.py console.py commands.py plugins.py modules/virustotal.py out.py colors.py 
+
+52. viper.py console.py commands.py plugins.py modules/virustotal.py abstracts.py colors.py
+
+53. viper.py console.py commands.py plugins.py modules/virustotal.py session.py out.py colors.py
+
+54. viper.py console.py commands.py plugins.py modules/virustotal.py session.py objects.py
+
+55. viper.py console.py commands.py plugins.py modules/yarascan.py out.py colors.py 
+
+56. viper.py console.py commands.py plugins.py modules/yarascan.py abstracts.py colors.py
+
+57. viper.py console.py commands.py plugins.py modules/yarascan.py database.py out.py colors.py
+
+58. viper.py console.py commands.py plugins.py modules/yarascan.py database.py objects.py
+
+59. viper.py console.py commands.py plugins.py modules/yarascan.py session.py out.py colors.py
+
+60. viper.py console.py commands.py plugins.py modules/yarascan.py session.py objects.py
+
+61. viper.py console.py commands.py plugins.py modules/yarascan.py storage.py
+
+62. viper.py console.py commands.py database.py out.py colors.py 
+
+63. viper.py console.py commands.py database.py objects.py
+
+64. viper.py console.py commands.py storage.py
